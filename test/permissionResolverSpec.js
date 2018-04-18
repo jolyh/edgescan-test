94030f4eb03fe114ddf813d2fa432cdbeb5e040b
joly_h <joly_h@bitbucket.org>
2018-04-18 22:06:34 +0200

    tested class is the same as before, passed all tests anyway :)

--- test/permissionResolverSpec.js
+++ test/permissionResolverSpec.js
@@ -1,4 +1,4 @@
-var expect = require('expect.js')
+var expect = require('expect')
 var PermissionDependencyResolver = require('../lib/permissionDependencyResolver')
 
 describe('PermissionDependencyResolver', function() {
@@ -18,49 +18,49 @@ describe('PermissionDependencyResolver', function() {
 
   it('validates whether permissions can be granted given simple dependencies', function(){
     pdr = new PermissionDependencyResolver(simplePermissionDependencies)
-    expect(pdr.canGrant(['view'], 'edit')).to.be.ok()
-    expect(pdr.canGrant(['view'], 'delete')).to.not.be.ok()
-    expect(pdr.canGrant(['view', 'edit'], 'alter_tags')).to.be.ok()
-    expect(pdr.canGrant(['view'], 'create')).to.be.ok()
+    expect(pdr.canGrant(['view'], 'edit')).toBeTruthy()
+    expect(pdr.canGrant(['view'], 'delete')).toBeFalsy()
+    expect(pdr.canGrant(['view', 'edit'], 'alter_tags')).toBeTruthy()
+    expect(pdr.canGrant(['view'], 'create')).toBeTruthy()
   })
 
   it('can sort permissions in dependency order given simple dependencies', function(){
     pdr = new PermissionDependencyResolver(simplePermissionDependencies)
 
-    expect(pdr.sort(['edit', 'delete', 'view'])).to.be.ok(['view', 'edit', 'delete'])
+    expect(pdr.sort(['edit', 'delete', 'view'])).toEqual(['view', 'edit', 'delete'])
 
     possible_orderings = [
       ['view', 'edit', 'create', 'alter_tags'],
       ['view', 'create', 'edit', 'alter_tags']
     ]
     // either of the possible orderings are valid for this input
-    expect(possible_orderings).to.be.ok(pdr.sort(['create', 'alter_tags', 'view', 'edit']))
+    expect(possible_orderings).toContainEqual(pdr.sort(['create', 'alter_tags', 'view', 'edit']))
   })
 
   it('validates whether permissions can be denied given simple dependencies', function(){
     pdr = new PermissionDependencyResolver(simplePermissionDependencies)
 
-    expect(pdr.canDeny(['view', 'edit'], 'view')).to.not.be.ok()
-    expect(pdr.canDeny(['view', 'edit'], 'edit')).to.be.ok()
-    expect(pdr.canDeny(['view', 'edit', 'create'], 'edit')).to.be.ok()
-    expect(pdr.canDeny(['view', 'edit', 'delete'], 'edit')).to.not.be.ok()
+    expect(pdr.canDeny(['view', 'edit'], 'view')).toBeFalsy()
+    expect(pdr.canDeny(['view', 'edit'], 'edit')).toBeTruthy()
+    expect(pdr.canDeny(['view', 'edit', 'create'], 'edit')).toBeTruthy()
+    expect(pdr.canDeny(['view', 'edit', 'delete'], 'edit')).toBeFalsy()
   })
 
   it('validates whether permissions can be granted given complex dependencies', function(){
     pdr = new PermissionDependencyResolver(complexPermissionDependencies)
 
-    expect(pdr.canGrant(['view', 'edit', 'delete'], 'batch_update')).to.not.be.ok()
-    expect(pdr.canGrant(['view', 'edit', 'create'], 'batch_update')).to.be.ok()
-    expect(pdr.canGrant(['view', 'edit', 'delete'], 'audit')).to.not.be.ok()
-    expect(pdr.canGrant(['view', 'edit', 'delete', 'create'], 'audit')).to.be.ok()
+    expect(pdr.canGrant(['view', 'edit', 'delete'], 'batch_update')).toBeFalsy()
+    expect(pdr.canGrant(['view', 'edit', 'create'], 'batch_update')).toBeTruthy()
+    expect(pdr.canGrant(['view', 'edit', 'delete'], 'audit')).toBeFalsy()
+    expect(pdr.canGrant(['view', 'edit', 'delete', 'create'], 'audit')).toBeTruthy()
   })
 
   it('throws an exception when validating permissions if existing permissions are invalid', function(){
     pdr = new PermissionDependencyResolver(complexPermissionDependencies)
 
-    expect(function () { pdr.canGrant(['edit', 'create'], 'alter_tags') }).to.throwError("Invalid Base Permissions")
-    expect(function () { pdr.canGrant(['view', 'delete'], 'alter_tags') }).to.throwError("Invalid Base Permissions")
-    expect(function () { pdr.canDeny(['create', 'delete'], 'audit') }).to.throwError("Invalid Base Permissions")
+    expect(function () { pdr.canGrant(['edit', 'create'], 'alter_tags') }).toThrowError("Invalid Base Permissions")
+    expect(function () { pdr.canGrant(['view', 'delete'], 'alter_tags') }).toThrowError("Invalid Base Permissions")
+    expect(function () { pdr.canDeny(['create', 'delete'], 'audit') }).toThrowError("Invalid Base Permissions")
   })
 
   it('can sort permissions in dependency order given complex dependencies', function(){
@@ -71,7 +71,7 @@ describe('PermissionDependencyResolver', function() {
       ['view', 'edit', 'delete', 'create', 'audit']
     ]
 
-    expect(possible_orderings).to.be.ok(pdr.sort(['audit', 'create', 'delete', 'view', 'edit']))
+    expect(possible_orderings).toContainEqual(pdr.sort(['audit', 'create', 'delete', 'view', 'edit']))
   })
 
 })
